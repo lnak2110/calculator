e139a94586aa741c80eaf05273dc8c5cd38cc5b8
akhang <lnak211099@gmail.com>
2022-04-12 21:30:21 +0700

    Create a function and eventlistener to display when the number buttons are clicked

--- script.js
+++ script.js
@@ -31,12 +31,29 @@ function operate(a, b, operator) {
     }
 }
 
-const digitsButtons = document.getElementById('digits-btn-container');
+const digitsButtonsContainer = document.getElementById('digits-btn-container');
+const displayContainer = document.getElementById('display-container');
 
 for (let i = 0; i <= 9; i++) {
     const digit = document.createElement('button');
-    digit.classList.add('btn');
+    digit.classList.add('btn', 'digit');
     digit.setAttribute('value', `${i}`);
     digit.textContent = `${i}`;
-    digitsButtons.appendChild(digit);
+    digitsButtonsContainer.appendChild(digit);
+}
+
+const digitsButtons = document.querySelectorAll('.digit');
+
+digitsButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        displayScreen(e.target.value);
+    });
+});
+
+const displayValue = document.createElement('div');
+displayValue.classList.add('display-value');
+displayContainer.appendChild(displayValue);
+
+function displayScreen(value) {
+    displayValue.textContent = value;
 }
